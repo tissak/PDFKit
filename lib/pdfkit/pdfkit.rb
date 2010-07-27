a8722a59cf85c0c8d6119c8c2e3c53540cd98554
Tony Issakov <tissakov@thefrontiergroup.com.au>
2010-07-27 13:21:13 +0800

    point to linux version of wkhtmltopdf

--- lib/pdfkit/pdfkit.rb
+++ lib/pdfkit/pdfkit.rb
@@ -35,7 +35,7 @@ class PDFKit
     case Config::CONFIG['host_os']
     when /x86_64-linux/i, /linux/i
       # insert xvfb if we are on a linux headless env
-      args = ['xvfb-run  -a -s \'-screen 0 640x480x16\' '].concat(args)
+      args = ['xvfb-run  -a -s \'-screen 0 640x480x16\' /usr/bin/wkhtmltopdf']
     end
 
     args += @options.to_a.flatten.compact
